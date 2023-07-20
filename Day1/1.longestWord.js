1b7b0ee4c98da87df1e717949204f3de1eb7b6db
sanket <sanketkopnar1299@gmail.com>
2023-07-20 16:58:34 +0530

    second last commit

--- Day1/1.longestWord.js
+++ Day1/1.longestWord.js
@@ -5,15 +5,16 @@
  *     Algo
  *
  *
- * 1] first i have to split the string and store into a variable  i.e newarr
- * 2] declare a variable outside the loop which store the first index i.e newarr2= newarr[0]
+ * 1] first i have to split the string and store into a variable  i.e newstr
+ * 2] declare a variable inside the loop which is blank
  * 3]Create a function
- * 4] inside the function run a loop till the length of arr i.e arr.length
- * 5]inside the loop create a variable which stores the all indexes i,e st=n[i]
+ * 4] inside the function run a loop till the length of newstr i.e newstr.length
+ * 5]inside the loop create a variable which stores the all indexes i,e st=newstr[i]
  * 6] run a condition if my curent variable length is grater than the outside variable lenthe  i.e
- *    st.length>newarr2.length
- * 7] then simply store into a my outside variabl
+ *    st.length>forstorestr.length
+ * 7] then simply store into a my outside variable
  * 8] last return that variable
+ * 9] that is forstorestr
  */
 
 let wordstr = "My name is ankit jain & i'm from morena";
